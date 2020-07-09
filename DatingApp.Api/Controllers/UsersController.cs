dae0ba383774f03284721fae192dd04686965d5c
west1989 <gigiabakelia@gmail.com>
2020-07-09 09:52:29 +0400

    UpdateUsers

--- DatingApp.Api/Controllers/UsersController.cs
+++ DatingApp.Api/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.Api.Data;
@@ -43,6 +44,22 @@ namespace DatingApp.Api.Controllers
       var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
       return Ok(userToReturn);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+    {
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+      if (id != int.Parse(userId))
+        return Unauthorized();
+
+      var userFromRepo = await _repo.GetUser(id);
+      _mapper.Map(userForUpdateDto, userFromRepo);
+
+      if (await _repo.SaveAll())
+        return NoContent();
+
+      throw new Exception($"Updating user {id} failed on save");
     }
 
   }
