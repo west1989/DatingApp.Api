8fc5202a4075f43b802d41d1446326d7dbebc2ef
west1989 <gigiabakelia@gmail.com>
2020-07-12 22:20:20 +0400

    Action Filter (Check User Last Activation)

--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -59,6 +59,8 @@ namespace DatingApp.Api
           ValidateAudience = false
         };
       });
+      services.AddScoped<LogUserActivity>();
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
